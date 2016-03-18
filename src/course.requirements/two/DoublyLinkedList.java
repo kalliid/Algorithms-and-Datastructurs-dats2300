3cdc433a07f1082432a64589b563c11359a928a3
osvold <osvold89@gmail.com>
2016-03-18 21:15:40 +0100

    Requirement #2

--- src/course.requirements/two/DoublyLinkedList.java
+++ src/course.requirements/two/DoublyLinkedList.java
@@ -1,6 +1,7 @@
 package course.requirements.two;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 /**
  * Created by hans on 18.03.16.
@@ -74,27 +75,108 @@ public class DoublyLinkedList<T> implements List<T>  {
 
     @Override
     public boolean add(T value) {
-        return false;
+
+        Objects.requireNonNull(value);
+
+        if(size == 0)
+        {
+            head = new Node<>(value, null, null);
+            tail = head;
+            size++;
+            numberOfChanges++;
+            return true;
+        }
+
+        Node<T> temp = head;
+
+        while(temp.next != null) temp = temp.next;
+
+        temp.next = new Node<>(value, temp, null);
+        tail = temp.next;
+
+        numberOfChanges++;
+        size++;
+
+        return true;
     }
 
     @Override
     public void insert(int index, T value) {
+        Objects.requireNonNull(value);
 
+        if(index < 0 || ((head == null) && index > 0) || index > size)
+            throw new IndexOutOfBoundsException("Error");
+
+        if(index == 0 && size == 0)
+        {
+
+            head = new Node<>(value, null, null);
+            tail = head;
+            size++;
+            numberOfChanges++;
+
+        } else if(index == size){
+
+            tail.next = new Node<>(value, tail, null);
+            tail = tail.next;
+            size++;
+            numberOfChanges++;
+
+        } else if(index == 0){
+
+            Node<T> temp = head;
+
+            head = new Node<>(value, null, head);
+            head.next.previous = head;
+
+            size++;
+            numberOfChanges++;
+
+        } else {
+
+            Node<T> temp = head;
+            Node<T> temp2 = head.next;
+            int i = 1;
+            while(i < index)
+            {
+                i++;
+                temp = temp.next;
+                temp2 = temp2.next;
+            }
+
+            temp.next = new Node<>(value, temp, temp.next);
+            temp2.previous = temp.next;
+
+            size++;
+            numberOfChanges++;
+        }
     }
 
     @Override
     public boolean contains(T value) {
-        return false;
+        return indexTo(value) != -1;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        indexControl(index, false);
+        return findNode(index).value;
     }
 
     @Override
     public int indexTo(T value) {
-        return 0;
+        if(value == null)
+            return -1;
+
+        Node<T> p = head;
+
+        for(int i = 0; i < size; i++)
+        {
+            if(p.value.equals(value)) return i;
+            p = p.next;
+        }
+
+        return -1;
     }
 
     @Override
